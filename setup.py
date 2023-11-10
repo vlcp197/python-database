bdbb62ef77bd229bb7b5c0af72aa0dd046c00a6a
vlcp197 <vlcp197@gmail.com>
2023-11-10 17:12:52 -0300

    Created a setup module to understand the module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    # original_author = 'Taavi Burns',
+    # original_author_email = 'taavi.burns@points.com',
+    # original_author_github =
+    # 'https://github.com/aosabook/500lines/tree/master/data-store'
+    name='dbdb',
+    description='Working on a database from scratch to learn how to build one',
+    url=r'https://github.com/vlcp197/python-database',
+    author_email='vlcp197@gmail.com',
+    packages=['dbdb']
+)
